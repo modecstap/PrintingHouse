e26460f046c2a799d7296b4379a68c63a7c0292d
Shebanin <toni.shebanin@mail.ru>
2025-09-23 16:46:53 +0300

    данные для конфигурации сервера передаются через переменные окружения

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,17 @@
 import asyncio
+import os
 
 from server.fast_api_server import FastAPIServer
 from server.server_config import ServerConfig
 
 
 async def main():
+    host = os.getenv("SERVER_HOST", "localhost")
+    port = int(os.getenv("SERVER_PORT", 8080))
+
     config = ServerConfig(
-        host="localhost",
-        port=8080
+        host=host,
+        port=port
     )
     server = FastAPIServer(
         config=config
