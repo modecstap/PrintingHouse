88336738895b9d961547ef2e61d2639852b9f8fa
Shebanin <toni.shebanin@mail.ru>
2025-12-29 14:48:57 +0300

    добавлена система миграций alembic

--- backend/storage/db_config.py
+++ backend/storage/db_config.py
@@ -1,3 +1,5 @@
+from urllib.parse import quote_plus
+
 from pydantic import BaseModel
 
 
@@ -7,3 +9,9 @@ class DBConfig(BaseModel):
     host: str
     port: int
     db_name: str
+
+    def get_db_url(self, driver: str):
+        if not self.password:
+            return f'postgresql+{driver}://{self.user}@{self.host}:{self.port}/{self.db_name}?target_session_attrs=read-write'
+        return f'postgresql+{driver}://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}'
+
