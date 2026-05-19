c7039b10244b5d87c01e350b683b257a8ec629bf
Shebanin <toni.shebanin@mail.ru>
2026-05-19 12:19:20 +0300

    feat(core): add check list size depending on die_cutting

--- backend/models/edition.py
+++ backend/models/edition.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 from backend.models.chroma import Chroma
 from backend.models.lamination import Lamination
@@ -19,3 +19,15 @@ class Edition(BaseModel):
     chroma: Chroma = Field(description="Цветность бумаги")
     lamination: Lamination = Field(description="Ламинация")
     die_cutting: bool = Field(description="Наличие высечки")
+
+    @model_validator(mode="after")
+    def validate_list_size(self):
+        if self.die_cutting:
+            min_size = 5
+        else:
+            min_size = 40
+
+        if self.list_size.height < min_size or self.list_size.width < min_size:
+            raise ValueError(f"Сторона листа должна быть не менее: {min_size}")
+
+        return self
--- backend/models/list_size.py
+++ backend/models/list_size.py
@@ -4,10 +4,10 @@ from pydantic import BaseModel, Field
 class ListSize(BaseModel):
     height: int = Field(
         description="Высота изделия в мм.",
-        ge=40,
+        ge=0,
     )
     width: int = Field(
         description="Ширина изделия в мм.",
-        ge=40,
+        ge=0,
     )
     bleeds: int
